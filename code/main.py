c473e496b3b7020b4ee38428c8eb3e18d69a8334
chrinilo <christian.louka@icloud.com>
2025-11-14 08:51:34 +0100

    documented a little

--- code/main.py
+++ code/main.py
@@ -9,29 +9,36 @@ import pygame
 window = pygame.display.set_mode((400,400))
 
 class Main_handel:
+    """
+    inholder alt.
+    """
     def __init__(self) -> None:
         self.window = pygame.display.set_mode((400,400))
         self.running= True # om main loopen skal kjøre
 
     def event_handel(self, ev):
+        """runs pygame events
+
+        Args:
+            ev (list): list of all events in pygame and wether they are true or false.
+        """
         if ev.type == pygame.QUIT:
             self.running = False
-            
-    
-    def name(args):
+
+    def name(self):
+        """idk
+        """
         pass
-    
 
     def main(self):
         """funksjonen som skal inholde alt av påkaling for at ting går framover.
         """
-        
+
         while self.running:
             event = pygame.event.wait()
             self.event_handel(event)
-            
+
             pygame.display.flip()
 
 main = Main_handel()
 main.main()
-pygame.quit()
