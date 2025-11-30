e82462e50f183d24eead3127bdbec7c5b1aa8814
christian <christian.louka@icloud.com>
2025-11-30 23:27:41 +0100

    aahhhh

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # værmelding-for-skole
 
-et program som skal vise værmeldingen på en ordentlig måte.
+this is a schoolproject
 
-dette er et skole prosjekt så vær fårbrett på at dette ikke er lagd helt fantastisk.
+**!!! the program will freeze when you press a button !!!**
+
+to run if you are on windows you can run the .exe file.
+
+<details>
+<summary>non windows OS</summary>
+if you do not have windows make sure you have python3.12 or newer
+make a python venv using the command
+
+```powershell
+python -m venv venv
+```
+
+install python_weather using pip:
+
+```powershell
+pip install python-weather
+```
+
+or
+
+```powershell
+python -m pip install python-weather
+```
+
+then open up the directory named code_tkinter and run the file main.py
+</details>
+
+to get the weather you press one of the three buttons on the top og the screen
+
+to search you in to one of the buttons and use the textbox to search you press søk
+this will search and refreash the curent temps.
--- code_tkinter/main.py
+++ code_tkinter/main.py
@@ -9,7 +9,7 @@ class Weather:
     
     async def weather_make(self):
         async with python_weather.Client() as client:
-            weather = await client.get("halden")
+            weather = await client.get(location=self.location)
             return weather
 
     async def temp_now(self):
@@ -22,6 +22,11 @@ class Weather:
     async def feels_like_now(self):
         weather = await self.weather_make()
         return weather.feels_like
+    
+    async def get_city(self):
+        weather = await self.weather_make()
+        return weather.location
+        
     
     async def forcast(self):
         weather = await self.weather_make()
@@ -44,11 +49,13 @@ class Frame1(tk.Frame):
     def __init__(self, parent_app):
         super().__init__(parent_app)
         self._location = tk.StringVar(self, "halden")
-        self.weather = Weather(self._location.get())
+        self.weather = Weather("oslo")
         self.parent = parent_app
+        
+        self.curent_viewscreen = int
     
     
-    def delete_children(self):
+    def delete_children(self) -> None:
         """
         deletets all lables and buttons on the screen.
         """
@@ -58,22 +65,38 @@ class Frame1(tk.Frame):
                 ch.destroy()
             c.destroy()
     
-    def city_textbox(self):
+    def city_textbox(self) -> None:
         """
         makes the textbox for which city you want to .
         """
         self.text_box_lable = tk.Label(self, text="by navn:")
         self.text_box_lable.grid(row=4, column=0)
-        self.text_box = tk.Text(self, height=1, width=5)
-        print()
-        self.text_box.grid(row=4,column=1)
-    
-    def change_city(self):
-        # TODO get to run when losing focus on the textbox
-        print(self.text_box.get('1.0',tk.END))
         
+        self.text_box = tk.Text(self, height=1, width=5)
+        self.text_box.grid(row=4,column=1)
+        
+        self.text_box_button = tk.Button(self, text="søk", command=lambda:self.change_city())
+        self.text_box_button.grid(row=4, column=2)
+    
+    def change_city(self) -> None:
+        print(self.text_box.get('1.0',tk.END).replace('\n', ''))
+        try:
+            self.weather = Weather ( self.text_box.get ( '1.0',tk.END ).replace ( '\n', '' ) )
+        except:
+            self.weather = Weather ("oslo")
+        finally:
+            match self.curent_viewscreen:
+                case 1:
+                    self.viewscreen_1()
+                    print('1')
+                case 2:
+                    self.viewscreen_2()
+                    print('2')
+                case 3:
+                    self.viewscreen_3()
 
-    def viewscreen_1(self):
+
+    def viewscreen_1(self) -> None:
         """
         loads everything for the temprature now button.
         """
@@ -82,13 +105,14 @@ class Frame1(tk.Frame):
         self.weather_text = tk.Label(self, text=f"{get_temp_now}")
         self.weather_text.grid(row=1,column=0,padx=(200,0), pady=(10,5),columnspan=1) # 
         
-        self.text_box_lable = tk.Label(self, text="by navn:")
-        self.text_box_lable.grid(row=2, column=0)
-        self.text_box = tk.Text(self, height=1, width=5)
-        self.text_box.grid(row=2,column=1)
+        tk.Label(self, text=asyncio.run(self.weather.get_city())).grid(row=2, column=0)
+        
+        self.city_textbox()
         
         self.pack(fill="x", expand=True)
-    def viewscreen_2(self):
+        self.curent_viewscreen = 1
+        
+    def viewscreen_2(self) -> None:
         """
         loads the curent forcast in three hour intervals.
         """
@@ -96,20 +120,32 @@ class Frame1(tk.Frame):
         
         forcast = asyncio.run(self.weather.forcast())
         tk.Label(self, text="klokken:").grid(row=0, column=0)
+        # prints tha lables for the forcast
         for i in range(len(forcast)):
             for j in range(len(forcast[i])):
                 tk.Label(self, text=(j*3)).grid(row=i, column=j+1)
             tk.Label(self, text=f"dag: {(i+1)}").grid(row=i+1, column=0)
-            
+        # prints the forcast for the next three days
         for i in range(len(forcast)):
             for j in range(len(forcast[i])):
                 tk.Label(self, text=f"{forcast[i][j]}").grid(row=i+1,column=j+1,padx=10, pady=(10,5),columnspan=1)
+        # makes the textbox
+        self.city_textbox()
+        self.pack(fill="x", expand=True)
+        self.curent_viewscreen = 2
+    
+    def viewscreen_3(self) -> None:
+        self.delete_children()
+        get_temp_now = asyncio.run(self.weather.feels_like_now())
+        self.weather_text = tk.Label(self, text=f"{get_temp_now}")
+        self.weather_text.grid(row=1,column=0,padx=(200,0), pady=(10,5),columnspan=1) # 
+        
+        tk.Label(self, text=asyncio.run(self.weather.get_city())).grid(row=2, column=0)
         
         self.city_textbox()
         
         self.pack(fill="x", expand=True)
-        if self.text_box.focus_displayof():
-            self.change_city()
+        self.curent_viewscreen = 3
 
 class Frame2(tk.Frame):
     def __init__(self,parent_app, weather_frame):
@@ -121,6 +157,8 @@ class Frame2(tk.Frame):
         """
         loads the buttons to change the viewscreen
         """
+        
+
         tk.Radiobutton(self, text="tempratur nå", value=1, variable=self.which_viewscreen, indicatoron=False, bg="white",
                         command=lambda:(self.weather.viewscreen_1())).grid(row=2,column=1, padx=0, pady=(30,0  ))
         tk.Radiobutton(self, text="temp 3 dager", value=2, variable=self.which_viewscreen, indicatoron=False, bg="white",
@@ -128,8 +166,6 @@ class Frame2(tk.Frame):
         tk.Radiobutton(self, text="humidety", value=3, variable=self.which_viewscreen, indicatoron=False, bg="white").grid(row=2,column=3, padx=0, pady=(30,0))
         self.pack()
 
-
-# weather = 1
 app = TK()
 frame1 = Frame1(app)
 frame2 = Frame2(app, frame1)
